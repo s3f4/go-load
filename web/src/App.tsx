6f009b3130e79045dadc043edc9ebfb9cefd180b
Sefa Şahin <sefa@dehaa.com>
2020-08-31 23:27:24 +0300

    index page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,11 +3,15 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Instances from "./pages/instaces";
 import Workers from "./pages/instaces";
 import Stats from "./pages/stats";
+import Index from "./pages";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
+        <Route exact path="/">
+          <Index />
+        </Route>
         <Route exact path="/instances">
           <Instances />
         </Route>
---
+++ web/src/pages/index.tsx
@@ -0,0 +1,18 @@
+/** @jsx jsx */
+import React from "react";
+import { jsx, css } from "@emotion/core";
+import MainLayout from "../components/layouts/MainLayout";
+
+interface Props {}
+
+const Index = (props: Props) => {
+  return (
+    <React.Fragment>
+      <MainLayout />
+    </React.Fragment>
+  );
+};
+
+const index = css``;
+
+export default Index;

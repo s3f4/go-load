c15026ce2e7438fce4533de468efdf29460f8373
Sefa Şahin <sefa@dehaa.com>
2020-09-12 13:22:38 +0300

    web button changes

--- web/src/components/basic/Button.tsx
+++ web/src/components/basic/Button.tsx
@@ -19,15 +19,21 @@ const Button: React.FC<Props> = (props: Props) => {
 };
 
 const button = css`
+  display: inline-block;
   border: ${Borders.border1};
+  min-width: 10rem;
+  min-height: 4rem;
   color: white;
   background-color: #007d9c;
   border-radius: ${Sizes.borderRadius1};
   padding: 1rem;
-  margin: 0.5rem auto;
+  margin: 0.5rem 0.5rem;
   font-size: 1.6rem;
   font-weight: 600;
   cursor: pointer;
+  &:hover {
+    background-color: #4caf50;
+  }
 `;
 
 export default Button;
--- web/src/pages/workers.tsx
+++ web/src/pages/workers.tsx
@@ -1,6 +1,4 @@
-/** @jsx jsx */
 import React from "react";
-import { jsx, css } from "@emotion/core";
 import MainLayout from "../components/layouts/MainLayout";
 import { listWorkers, stopWorker } from "../api/api";
 import { Worker } from "../api/entity/worker";

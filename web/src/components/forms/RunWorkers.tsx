43ac3cacf0671be4dba440c114e0faf50e947a8c
Sefa Şahin <sefa@dehaa.com>
2020-09-20 13:55:36 +0300

    run workers redirect to stats

--- web/src/components/forms/RunWorkers.tsx
+++ web/src/components/forms/RunWorkers.tsx
@@ -6,6 +6,7 @@ import Button from "../basic/Button";
 import { toNum } from "../basic/helper";
 import { BaseForm } from "./BaseForm";
 import { runWorkers } from "../../api/entity/worker";
+import Loader from "../basic/Loader";
 
 interface Props extends BaseForm {}
 
@@ -13,6 +14,7 @@ const RunWorkers = (props: Props) => {
   const [requestCount, setRequestCount] = React.useState<number>(0);
   const [url, setUrl] = React.useState<string>("");
   const [goroutineCount, setGoroutineCount] = React.useState<number>(0);
+  const [loading, setLoading] = React.useState<boolean>(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     switch (e.target.name) {
@@ -30,41 +32,52 @@ const RunWorkers = (props: Props) => {
 
   const run = (e: React.FormEvent) => {
     e.preventDefault();
+    setLoading(true);
     runWorkers({
       requestCount,
       goroutineCount,
       url,
     })
-      .then((response) => console.log(response))
-      .catch((error) => console.log(error));
+      .then((response) => {
+        setLoading(false);
+        props.afterHandle?.();
+      })
+      .catch((error) => {
+        setLoading(false);
+        props.afterHandle?.();
+      });
   };
 
-  return (
-    <div css={formDiv}>
-      <h2 css={formTitle}>Run Workers</h2>
-      <TextInput
-        onChange={handleChange}
-        label="Target URL"
-        name="url"
-        value={url}
-      />
+  const formContent = () => {
+    return (
+      <div css={formDiv}>
+        <h2 css={formTitle}>Run Workers</h2>
+        <TextInput
+          onChange={handleChange}
+          label="Target URL"
+          name="url"
+          value={url}
+        />
 
-      <TextInput
-        onChange={handleChange}
-        label="Total Request"
-        name="requestCount"
-        value={requestCount}
-      />
+        <TextInput
+          onChange={handleChange}
+          label="Total Request"
+          name="requestCount"
+          value={requestCount}
+        />
 
-      <TextInput
-        onChange={handleChange}
-        label="Goroutine per worker (up to 10)"
-        name="goroutineCount"
-        value={goroutineCount}
-      />
-      <Button text="run" onClick={run} />
-    </div>
-  );
+        <TextInput
+          onChange={handleChange}
+          label="Goroutine per worker (up to 10)"
+          name="goroutineCount"
+          value={goroutineCount}
+        />
+        <Button text="run" onClick={run} />
+      </div>
+    );
+  };
+
+  return loading ? <Loader message="Workers are running..." /> : formContent();
 };
 
 const formDiv = css`
--- web/src/pages/instaces.tsx
+++ web/src/pages/instaces.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import MainLayout from "../components/layouts/MainLayout";
 import SpinUp from "../components/forms/SpinUpForm";
 import RunWorkers from "../components/forms/RunWorkers";
+import { useHistory } from "react-router-dom";
 
 interface Props {}
 
 const Instances: React.FC<Props> = (props: Props) => {
   const [showRunWorkerForm, setShowRunWorkerForm] = React.useState<boolean>();
+  const history = useHistory();
+
+  const routeToStats = () => {
+    history.push("/stats");
+  };
 
   const spinUpAfterHandle = () => {
     setShowRunWorkerForm(true);
@@ -17,7 +23,7 @@ const Instances: React.FC<Props> = (props: Props) => {
     <SpinUp afterHandle={spinUpAfterHandle} />
   );
 
-  const runWorkersForm = () => <RunWorkers />;
+  const runWorkersForm = () => <RunWorkers afterHandle={routeToStats} />;
 
   return (
     <React.Fragment>

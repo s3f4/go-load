911ac3551c762f205c74381ccf9bcd57451f83b5
Sefa Şahin <sefa@dehaa.com>
2020-09-20 22:41:49 +0300

    stats table changes

--- web/src/components/contents/StatsContent.tsx
+++ web/src/components/contents/StatsContent.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { jsx, css } from "@emotion/core";
 import { stats, Response } from "../../api/entity/stats";
+import moment from "moment";
 
 interface Props {}
 
@@ -21,36 +22,44 @@ const StatsContent: React.FC<Props> = (props: Props) => {
       .catch((error) => console.log(error));
   };
 
+  const format = (): string => {
+    return "hh:mm:ss SSSS";
+  };
+
+  const byteSize = (str: string) => new Blob([str]).size;
+
   return (
     <div css={statsContainer}>
-      <table>
+      <table css={table}>
         <thead>
-          <th>FirstByte</th>
-          <th>ConnectStart</th>
-          <th>ConnectDone</th>
-          <th>DNSStart</th>
-          <th>DNSDone</th>
-          <th>TLSStart</th>
-          <th>TLSDone</th>
-          <th>StatusCode</th>
-          <th>TotalTime</th>
-          <th>Body</th>
+          <tr>
+            <th>FirstByte</th>
+            <th>ConnectStart</th>
+            <th>ConnectDone</th>
+            <th>DNSStart</th>
+            <th>DNSDone</th>
+            <th>TLSStart</th>
+            <th>TLSDone</th>
+            <th>StatusCode</th>
+            <th>TotalTime</th>
+            <th>Body</th>
+          </tr>
         </thead>
         <tbody>
           {responses &&
             responses.map((response: Response, key: number) => {
               return (
                 <tr>
-                  <td>{response.FirstByte}</td>
-                  <td>{response.ConnectStart}</td>
-                  <td>{response.ConnectDone}</td>
-                  <td>{response.DNSStart}</td>
-                  <td>{response.DNSDone}</td>
-                  <td>{response.TLSStart}</td>
-                  <td>{response.TLSDone}</td>
-                  <td>{response.StatusCode}</td>
+                  <td>{moment(response.FirstByte).format(format())}</td>
+                  <td>{moment(response.ConnectStart).format(format())}</td>
+                  <td>{moment(response.ConnectDone).format(format())}</td>
+                  <td>{moment(response.DNSStart).format(format())}</td>
+                  <td>{moment(response.DNSDone).format(format())}</td>
+                  <td>{moment(response.TLSStart).format(format())}</td>
+                  <td>{moment(response.TLSDone).format(format())}</td>
+                  <td>{moment(response.StatusCode).format(format())}</td>
                   <td>{response.TotalTime / 1000000}</td>
-                  <td>{response.Body}</td>
+                  <td>{byteSize(response.Body)}</td>
                 </tr>
               );
             })}
@@ -62,5 +71,9 @@ const StatsContent: React.FC<Props> = (props: Props) => {
 };
 
 const statsContainer = css``;
+
+const table = css`
+  width: 100%;
+`;
 
 export default StatsContent;

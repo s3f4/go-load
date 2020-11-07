0fa29ca18e407424d22edc4272498a53ec74882a
Sefa Şahin <sefa@dehaa.com>
2020-11-07 15:35:40 +0300

    route, api changes

--- apigateway/app/routes.go
+++ apigateway/app/routes.go
@@ -35,42 +35,53 @@ func routeMap(*chi.Mux) {
 		router.Get("/signout", handlers.AuthHandler.Signout)
 	})
 
-	router.Post("/instances", handlers.InstanceHandler.SpinUp)
-	router.Get("/instances", handlers.InstanceHandler.GetInstanceInfo)
-	router.Get("/instances/regions", handlers.InstanceHandler.ShowRegions)
-	router.Get("/instances/account", handlers.InstanceHandler.ShowAccount)
-	router.Get("/instances/swarm-nodes", handlers.InstanceHandler.ShowSwarmNodes)
-	router.Delete("/instances", handlers.InstanceHandler.Destroy)
-	router.Get("/workers", handlers.WorkerHandler.List)
-	router.Post("/workers", handlers.WorkerHandler.Stop)
+	router.Group(func(router chi.Router) {
+		router.Route("/instances", func(router chi.Router) {
+			router.Post("/", handlers.InstanceHandler.SpinUp)
+			router.Get("/", handlers.InstanceHandler.GetInstanceInfo)
+			router.Get("/regions", handlers.InstanceHandler.ShowRegions)
+			router.Get("/account", handlers.InstanceHandler.ShowAccount)
+			router.Get("/swarm-nodes", handlers.InstanceHandler.ShowSwarmNodes)
+			router.Delete("/", handlers.InstanceHandler.Destroy)
+		})
 
-	router.Post("/test_group/{ID}/start", handlers.TestGroupHandler.Start)
-	router.Get("/test_group/{ID}", handlers.TestGroupHandler.List)
-	router.Post("/test_group", handlers.TestGroupHandler.Create)
-	router.Get("/test_group", handlers.TestGroupHandler.List)
-	router.Put("/test_group", handlers.TestGroupHandler.Update)
-	router.Delete("/test_group", handlers.TestGroupHandler.Delete)
+		router.Get("/workers", handlers.WorkerHandler.List)
+		router.Post("/workers", handlers.WorkerHandler.Stop)
 
-	router.Route("/test", func(router chi.Router) {
-		router.Post("/", handlers.TestHandler.Create)
-		router.Get("/", handlers.TestHandler.List)
+		router.Route("/test_group", func(router chi.Router) {
+			router.Post("/", handlers.TestGroupHandler.Create)
+			router.Get("/", handlers.TestGroupHandler.List)
+			router.Put("/", handlers.TestGroupHandler.Update)
+			router.Delete("/", handlers.TestGroupHandler.Delete)
 
-		router.Route("/{ID}", func(router chi.Router) {
-			router.Use(middlewares.TestCtx)
-			router.Post("/start", handlers.TestHandler.Start)
-			router.Get("/", handlers.TestHandler.Get)
-			router.Put("/", handlers.TestHandler.Update)
-			router.Delete("/", handlers.TestHandler.Delete)
+			router.Route("/{ID}", func(router chi.Router) {
+				router.Post("/start", handlers.TestGroupHandler.Start)
+				router.Get("/", handlers.TestGroupHandler.List)
+			})
+		})
+
+		router.Route("/test", func(router chi.Router) {
+			router.Post("/", handlers.TestHandler.Create)
+			router.Get("/", handlers.TestHandler.List)
+
+			router.Route("/{ID}", func(router chi.Router) {
+				router.Use(middlewares.TestCtx)
+				router.Post("/start", handlers.TestHandler.Start)
+				router.Get("/", handlers.TestHandler.Get)
+				router.Put("/", handlers.TestHandler.Update)
+				router.Delete("/", handlers.TestHandler.Delete)
+			})
+		})
+
+		router.Route("/run_test", func(router chi.Router) {
+			router.Route("/{ID}", func(router chi.Router) {
+				router.Use(middlewares.RunTestCtx)
+				router.Get("/", handlers.RunTestHandler.Get)
+				router.Delete("/", handlers.RunTestHandler.Delete)
+				router.Get("/stats", handlers.StatsHandler.List)
+			})
+			router.Get("/run_test", handlers.RunTestHandler.List)
 		})
 	})
 
-	router.Route("/run_test", func(router chi.Router) {
-		router.Route("/{ID}", func(router chi.Router) {
-			router.Use(middlewares.RunTestCtx)
-			router.Get("/", handlers.RunTestHandler.Get)
-			router.Delete("/", handlers.RunTestHandler.Delete)
-			router.Get("/stats", handlers.StatsHandler.List)
-		})
-		router.Get("/run_test", handlers.RunTestHandler.List)
-	})
 }
--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -1,4 +1,11 @@
 const URL = `http://${process.env.REACT_APP_API_BASE_URL}`;
+
+export interface ServerResponse {
+  status: boolean;
+  data?: any;
+  message?: string;
+}
+
 // makeReq makes requests
 export const makeReq = async (url: string, method?: any, body?: any) => {
   if (!url.startsWith("/")) {
@@ -20,22 +27,17 @@ export const makeReq = async (url: string, method?: any, body?: any) => {
   }
 
   return await fetch(request.url, request.config).then((response: Response) => {
-    return new Promise((resolve, reject) => {
-      if (response) {
-        if (response.ok) {
-          response.json().then((json) => resolve(json));
-        } else {
-          response
-            .json()
-            .then((json) => reject(json))
-            .catch(() =>
-              reject({
-                status: response.status,
-                message: response.statusText,
-              }),
-            );
-        }
-      }
+    return new Promise<ServerResponse>((resolve, reject) => {
+      response
+        .json()
+        .then((json: ServerResponse) => {
+          if (response.ok) {
+            resolve(json);
+          } else {
+            reject(json);
+          }
+        })
+        .catch((error) => reject({ status: response.status, message: error }));
     });
   });
 };
